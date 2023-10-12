6b7e01228899370b5ddb759954c4af206d5d352d
vvetamaxx <146852324+vvetamaxx@users.noreply.github.com>
2023-10-12 12:38:23 +0500

    Start project

---
+++ DP_Project/Assets/Scenes/DragonPicker.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonPicker : MonoBehaviour
+{
+    public GameObject energyShieldPrefab;
+    public int numEnergyShield = 3;
+    public float energyShieldBottomY = -6f;
+    public float energyShieldRadius = 1.5f;
+    
+    void Start()
+    {
+        for (int i = 1; i <=numEnergyShield; i++)
+        {
+            GameObject tShieldGo = Instantiate<GameObject>(energyShieldPrefab);
+            tShieldGo.transform.position = new Vector3(0, energyShieldBottomY, 0);
+            tShieldGo.transform.localScale = new Vector3(1 * i, 1 * i, 1 * i);
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    
+    public void DragonEggDestroyed()
+    {
+        GameObject[] tDragonEggArray = GameObject.FindGameObjectsWithTag("Dragon Egg");
+        foreach (GameObject tGO in tDragonEggArray)
+        {
+            Destroy(tGO);
+        }
+    }
+}
